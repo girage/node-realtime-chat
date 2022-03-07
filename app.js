b45c2366610a7a77d2869bd57fb65a5c5a4633c4
girage <45748977+girage@users.noreply.github.com>
2022-03-07 21:27:24 +0700

    connect socket and pass message

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
+
+
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 3050;
-
+const server = http.createServer(app);
+const io = socketio(server);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Run when client connect
+io.on('connection', socket => {
+
+  // Welcome user to the chat
+  socket.emit('message', 'Welcome to ChatCord!!');
+
+  // broadcast to user connects
+  socket.broadcast.emit('message', 'A user has joined the chat');
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat');
+  })
+
+  // Listen to chat message
+  socket.on('chatMessage', msg => {
+    io.emit('message', msg);
+  })
+
+})
 
 app.get('/', (req, res) => {
   res.send('Hello');
@@ -15,7 +42,7 @@ app.get('/', (req, res) => {
 
 const start = () => {
   try {
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`Listening for port: ${PORT}`);
     });
   } catch (error) {
